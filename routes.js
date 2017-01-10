894944b2141a2623adc31bf24958e7dbc4a25273
Princevlad <rapdownswitch@gmail.com>
2017-01-10 14:24:19 +0100

    all added

--- routes.js
+++ routes.js
@@ -1,14 +1,26 @@
-var cors = require('cors');
-var express  = require('express');
-var app      = express();
-var multer   = require('multer');
-var nodemailer = require('nodemailer');
+var cors          = require('cors');
+var express       = require('express');
+var app           = express();
+var multer        = require('multer');
+var nodemailer    = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-var hbs = require('nodemailer-express-handlebars');
-var passport = require('passport');
-var request = require('request');
-var db = require('./database');
-var Agent = db.Agent;
+var hbs           = require('nodemailer-express-handlebars');
+var passport      = require('passport');
+var request       = require('request');
+var db            = require('./database');
+
+
+
+// ==============         Db importation ========   //
+
+
+var Agent         = db.Agent;
+var user          = user,
+var Aggregator    = Aggregator,
+var Admin         = Admin,
+var Association   = Association,
+var Document      = Document,
+var Member        = Member;
 
 
 
@@ -41,6 +53,21 @@ res.render('Login',{info: req.flash('info')});
 });
 
 
+
+app.post('/Add', function(req, res){
+
+
+
+});
+
+
+
+
+
+
+
+
+
 app.post('/login', passport.authenticate('local-login', {
   successRedirect : '/Admin',
   failureRedirect : '/login',
@@ -54,15 +81,6 @@ app.post('/login', passport.authenticate('local-login', {
    failureFlash : true
  }));
 
-// app.post('/signup', function(req, res){
-
-//       res.send(req.body);
-//  });
-
-app.post('/test', function(req, res){
-    console.log(req.body);
-res.send(req.body);
-});
 
 app.get('/logout', function(req, res) {
   req.logout();
